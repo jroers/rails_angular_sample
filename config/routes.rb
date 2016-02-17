5c30427c38da64cd7b54bd665c0d87c39d8c19ed
jroers <joe.roers@gmail.com>
2016-02-17 09:26:49 -0800

    Added api route. Generated todo model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'site#index'
 
+  namespace :api, defaults: { format: :json } do
+    resources :todos, except: [:new, :edit]
+  end
+
   get '*path', to: 'site#index'
 
 end
