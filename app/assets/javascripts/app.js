ef6f3535aee5dd37eeecd0f09ba0eaa8a3014878
jroers <joe.roers@gmail.com>
2016-02-17 09:14:58 -0800

    Totes meant to screw up the syntax for

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,14 +2,14 @@ angular.module('SampleApp', ['ui.router', 'templates'])
 	.config(config)
 	.controller("HomeController", HomeController);
 
-config.$inject(["$stateProvider", "$urlRouterProvider"]);
+config.$inject = ["$stateProvider", "$urlRouterProvider"];
 function config($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('/');
 
 	$stateProvider
 		.state('home', {
 			url: '/',
-			templateUrl: 'home.html',
+			template: 'Hello',
 			controller: 'HomeController',
 			controllerAs: 'home'
 		});
@@ -17,7 +17,7 @@ function config($stateProvider, $urlRouterProvider) {
 
 // CONTROLLERS:
 
-HomeController.$inject([]);
+HomeController.$inject = [];
 function HomeController() {
 	var vm = this;
 	console.log("Home Controller!!");
