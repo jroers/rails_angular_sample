5334cccf6aecdca43a9d6e4eec1c588355947345
jroers <joe.roers@gmail.com>
2016-02-17 09:06:06 -0800

    added config and HomeController

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,25 @@
-angular.module('SampleApp', ['ui.router', 'templates']);
+angular.module('SampleApp', ['ui.router', 'templates'])
+	.config(config)
+	.controller("HomeController", HomeController);
+
+config.$inject(["$stateProvider", "$urlRouterProvider"]);
+function config($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'home.html',
+			controller: 'HomeController',
+			controllerAs: 'home'
+		});
+}
+
+// CONTROLLERS:
+
+HomeController.$inject([]);
+function HomeController() {
+	var vm = this;
+	console.log("Home Controller!!");
+	vm.homeTest = "Welcome to the homepage.";
+}
