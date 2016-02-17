541f2d7143ee8ae91f122fee33cb829fe6956de5
jroers <joe.roers@gmail.com>
2016-02-17 09:29:45 -0800

    made todos controller

---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,23 @@
+class Api::TodosController < ApplicationController
+	
+	def index
+		@todos = Todo.all.order("created_at DESC")
+		render json: @todos
+	end
+
+	def create
+		@todo = Todo.new(todo_params)
+		if @todo.save
+			render json: @todo
+		else
+			render json: { errors: @todo.errors.full_messages.join(", ") }, status: :unprocessable_entity
+		end
+	end
+
+	private
+
+	def todo_params
+		params.require(:todo).permit(:title, :description, :done)
+	end
+
+end
